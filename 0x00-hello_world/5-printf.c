30a36b694dd0f242e327a8285189eae9dce3d50f
Emeka-soft <chukwurex042@gmail.com>
2023-09-11 19:55:28 -0700

    hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the entry function that takes no argument
+ *
+ * Return: will return 0 when successful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
